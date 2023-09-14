225ebf1ce807b8bc425bbc4e6a2bafe609838f11
Wentana Ghile <wentanaghile@MacBook-Pro.local>
2023-09-14 13:24:21 -0400

    struggling

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { HashRouter, Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Form from './components/Form'
 import Results from './components/Results'
