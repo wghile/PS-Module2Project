864f538bd49ea650dade2392ab3f6409b76fff60
Wentana Ghile <wentanaghile@MacBook-Pro.local>
2023-09-14 12:22:44 -0400

    troubleshooting...

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { HashRouter, HashRouter as Router } from 'react-router-dom'
+// import { HashRouter, HashRouter as Router } from 'react-router-dom'
+import { HashRouter as Router } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <HashRouter>
+  <Router>
     <App />
-  </HashRouter>
+  </Router>
 )
